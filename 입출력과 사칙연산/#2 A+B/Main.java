b1ee07702843dc4e06de9a4d21d518609a91f04a
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2023-02-22 23:56:18 +0900

    Create Main.java

---
+++ 입출력과 사칙연산/#2 A+B/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner a = new Scanner(System.in);
+		
+		int A = a.nextInt();
+		int B = a.nextInt();
+		
+		System.out.print(A+B);
+	}
+}
